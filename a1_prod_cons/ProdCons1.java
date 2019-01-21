10d07e1d84ec0b8ae706e0ba585c70215e810073
Sascha Strand <sastrand@pdx.edu>
2019-01-20 17:58:56 -0800

    completed ProdCons1.java

--- a1_prod_cons/ProdCons1.java
+++ a1_prod_cons/ProdCons1.java
@@ -1,54 +1,70 @@
 //----------------------------------------------------------------------------- 
-// Program code for CS 415/515 Parallel Programming, Portland State University.
+// ProdCons1
+// A Java class that creates one producer and one consumer
+// that operate concurrently over a shared buffer
+// Sascha Strand
+// (c) 2019
 //----------------------------------------------------------------------------- 
 
-// A Java notify/wait demo program.
-//
-//
-public class SignalDemo {
+import java.util.LinkedList;
+import java.util.Queue;
 
-  // any object can server as synchronization object
+public class ProdCons1 {
   static String synObj = "synObj";
+  static Queue<Integer> buf = new LinkedList<Integer>();
+  static Integer CAP = 20;
 
-  static Runnable doNotify = new Runnable() {
-      public void run() {
-	String name = Thread.currentThread().getName();
-	System.out.println(name + " running");
-	synchronized (synObj) {
-	  try {
-	    synObj.notifyAll();
-	  } catch (Exception e) {
-	    System.err.println(e.getMessage());
-	  }
-	}
-	System.out.println(name + " signal sent");
+  static Runnable doProducer = new Runnable() {
+    public void run() {
+      System.out.println("Producer starting");
+      for (int i = 1; i <= 100; i++) {
+        synchronized (synObj) {
+          try {
+            if (buf.size() >= CAP) {
+              synObj.wait();
+            }
+            buf.add(i);
+            synObj.notifyAll();
+            System.out.println("Producer added value " + i + " (qsize = " 
+                              + buf.size() + ")");
+          } catch (Exception e) {
+            System.err.println(e.getMessage());
+          }
+        }
       }
-    };
+    }
+  };
 
-  static Runnable doWait = new Runnable() {
-      public void run() {
-	String name = Thread.currentThread().getName();
-	System.out.println(name + " running");
-	synchronized (synObj) {
-	  try {
-	    synObj.wait();
-	  } catch (Exception e) {
-	    System.err.println(e.getMessage());
-	  }
-	}
-	System.out.println(name + " signal received");
+  static Runnable doConsumer = new Runnable() {
+    public void run() {
+      Integer i = 0;
+      Integer i_count = 0;
+      System.out.println("Consumer starting");
+      while (i_count < 100) {
+        synchronized (synObj) {
+          try {
+            if (buf.size() < 1) {
+              synObj.wait();
+            }
+            i = buf.remove();
+            i_count = i_count + 1;
+            synObj.notifyAll();
+            System.out.println("Value " + i + " consumed");
+            } catch (Exception e) {
+              System.err.println(e.getMessage());
+          }
+        }
       }
-    };
+    }
+  };
 
   public static void main(String[] args) {
-    Thread notifier = new Thread(doNotify, "Notify");
-    Thread waiter1 = new Thread(doWait, "Wait 1");
-    Thread waiter2 = new Thread(doWait, "Wait 2");
+    Thread consumer = new Thread(doConsumer);
+    Thread producer = new Thread(doProducer);
     try {
-      waiter1.start();
-      waiter2.start();
-      Thread.sleep(100); // sleep for 100ms
-      notifier.start();
+      consumer.start();
+      Thread.sleep(100); // sleep for 1 second
+      producer.start();
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
