eecb654db0df972deb6d1992d0dbd7422a08cb09
sascha <sastrand@pdx.edu>
2019-01-21 08:51:29 -0800

    added CL input, thread joins at end

--- a1_prod_cons/ProdCons2.java
+++ a1_prod_cons/ProdCons2.java
@@ -9,13 +9,15 @@
 import java.util.*;
 
 public class ProdCons2 {
-  static String synObj = "synObj";
-  static Queue<Integer> buf = new LinkedList<Integer>();
-  static Integer CAP = 20;
-  static Integer consumedGlobalCount = 0;
-  static String [] consumerCount = new String[1000];
+  private static final String synObj = "synObj";
+  private static Queue<Integer> buf = new LinkedList<>();
+  private static final Integer CAP = 20;
+  private static Integer numCons = 1;
+  private static Integer consumedGlobalCount = 0;
+  private static String [] consumerCounts = new String[1000];
 
-  static Runnable doProducer = new Runnable() {
+  @SuppressWarnings("Duplicates")
+  static private Runnable doProducer = new Runnable() {
     public void run() {
       System.out.println("Producer starting");
       for (int i = 1; i <= 100; i++) {
@@ -36,7 +38,7 @@ public class ProdCons2 {
     }
   };
 
-  static Runnable doConsumer = new Runnable() {
+  static private Runnable doConsumer = new Runnable() {
     public void run() {
       String tName = Thread.currentThread().getName();
       Integer tid = Integer.parseInt(tName);
@@ -54,10 +56,10 @@ public class ProdCons2 {
                 break;
               }
             }
-            if (end != true) {
+            if (!end) {
               i = buf.remove();
               consumedGlobalCount = consumedGlobalCount + 1;
-              consumerCount[tid] = consumerCount[tid] + 1;
+              consumerCounts[tid] = consumerCounts[tid] + 1;
               synObj.notifyAll();
               System.out.println("Consumer[" + Thread.currentThread().getName()
                                  + "] removed value " + i);
@@ -71,14 +73,46 @@ public class ProdCons2 {
             System.err.println(e.getMessage());
           }
         }
-        if (end == true) break;
+        if (end) break;
       }
     }
   };
 
+  private static Integer getNumCons(String[] args) {
+    String usingDefault = "Using the default number of consumers, 1.";
+    if (args.length < 1) {
+      System.out.println(usingDefault);
+      return 1;
+    } else if (args.length > 1) {
+      System.out.println("Please supply only one argument.\n" + usingDefault);
+      return 1;
+    } else {
+      Integer numCons = Integer.parseInt(args[0]);
+      if (numCons > 1 && numCons < 1000) {
+        return numCons;
+      } else {
+        System.out.println("Please supply a number of consumers greater than 1 and less than 1000.\n"
+                           + usingDefault);
+      }
+    }
+    return 1;
+  }
+
+  private static void printConsumerCounts() {
+    Integer sum = 0;
+    for (int i=0;i<numCons;i++) {
+      if (i<numCons-1) {
+        System.out.printf("C[%d]:%d, ", i, -1);
+      } else {
+        System.out.printf("C[%d]:%d\n", i, -1);
+      }
+      //sum = sum + Integer.parseInt(consumerCounts[i]);
+    }
+  }
+
   public static void main(String[] args) {
-    Integer numCons = 9;
-    ArrayList<Thread> threads = new ArrayList<Thread>();
+    numCons = getNumCons(args);
+    ArrayList<Thread> threads = new ArrayList<>();
     for (int i = 0; i < numCons; i++) {
       threads.add(new Thread(doConsumer, Integer.toString(i)));
     }
@@ -89,10 +123,15 @@ public class ProdCons2 {
       } 
       Thread.sleep(100); // sleep for 1 second
       producer.start();
+      for (int i = 0; i < numCons; i++) {
+        threads.get(i).join();
+        System.out.printf("Joined %d\n", i);
+      }
+      printConsumerCounts();
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
-    System.out.println("Done!");
+    System.out.println("Main: all done!");
   }
 
 }
