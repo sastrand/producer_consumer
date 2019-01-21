534c3b963c759c648eb46b32e0325567e4ae61bf
Sascha Strand <sastrand@pdx.edu>
2019-01-20 16:35:01 -0800

    prodcons-pthd2.c takes CL args, runs with 1 consumer

--- a1_prod_cons/prodcons-pthd2.c
+++ a1_prod_cons/prodcons-pthd2.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <unistd.h> 
 #include <sched.h>
+#include <stdlib.h>
 #include "queue.h"
 
 pthread_mutex_t lock;
@@ -28,9 +29,9 @@ void producer() {
   }
 }
 
-void consumer() {
+void consumer(long tid) {
   int i = 0, i_count = 0;
-  printf("Consumer starting on core %2d\n", sched_getcpu());
+  printf("Consumer[%3ld] starting on core %2d\n", tid, sched_getcpu());
   while(i_count < 100) {
     pthread_mutex_lock(&lock);
     if (buf->size<1) pthread_cond_wait(&cond, &lock);
@@ -42,14 +43,36 @@ void consumer() {
   }
 }
 
+int get_numCons(int argc, char **argv) {
+  const char default_numCons[] = "Using default value of 1 for the number of "
+                          "consumers. Input must be between 1 and 999.\n";
+  if (argc<=1) {
+    printf(default_numCons);
+    return 1;
+  } else {
+    int ret = atoi(argv[1]);
+    if (ret < 1 || ret > 999) {
+      printf(default_numCons);
+      return 1;
+    } else {
+      return ret;
+    }
+  }
+}
+
 int main(int argc, char **argv) {
-  pthread_t tid1;
-  long tids[1] = {1};
+  int numCons = get_numCons(argc, argv);
+  pthread_t threads[numCons];
+  long tids[numCons];
   buf = create_queue(CAP);
   pthread_mutex_init(&lock, NULL);
   pthread_cond_init(&cond, NULL);
-  pthread_create(&tid1, NULL, (void *)consumer, NULL);
+  for (long i=0;i<numCons;i++) {
+    pthread_create(&threads[i], NULL, (void *)consumer, (void*)i);
+  }
   producer();
-  pthread_join(tid1, NULL);
+  for (int i=0;i<numCons;i++) {
+    pthread_join(threads[i], NULL);
+  }
 }
 
