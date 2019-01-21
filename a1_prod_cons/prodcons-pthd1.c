c90ca0b380c07c97aed8e2d23af386bcd58f3747
Sascha Strand <sastrand@pdx.edu>
2019-01-20 16:15:00 -0800

    finished prodcons-pthd1.c

--- a1_prod_cons/prodcons-pthd1.c
+++ a1_prod_cons/prodcons-pthd1.c
@@ -3,6 +3,7 @@
  *---------------------------------------------------------------------------*/
 
 #define _GNU_SOURCE
+#define CAP 20
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h> 
@@ -18,9 +19,11 @@ void producer() {
   sleep(1);
   for (int i=1;i<=100;i++) {
     pthread_mutex_lock(&lock);
+    if (buf->size>=CAP) pthread_cond_wait(&cond, &lock);
     add_item(buf, i);
     pthread_cond_signal(&cond);
-    printf("Producer added value %3d (qsize = %2d). Signaled.\n", i, buf->size);
+    printf("Producer added value %3d"
+           " (qsize = %2d) Signaled\n", i, buf->size);
     pthread_mutex_unlock(&lock);
   }
 }
@@ -30,10 +33,11 @@ void consumer() {
   printf("Consumer starting on core %2d\n", sched_getcpu());
   while(i_count < 100) {
     pthread_mutex_lock(&lock);
-    pthread_cond_wait(&cond, &lock);
+    if (buf->size<1) pthread_cond_wait(&cond, &lock);
     i = remove_item(buf);
     i_count++;
-    printf("Value %3d consumed.\n", i);
+    pthread_cond_signal(&cond);
+    printf("Value %3d consumed\n", i);
     pthread_mutex_unlock(&lock);
   }
 }
@@ -41,7 +45,7 @@ void consumer() {
 int main(int argc, char **argv) {
   pthread_t tid1;
   long tids[1] = {1};
-  buf = create_queue(20);
+  buf = create_queue(CAP);
   pthread_mutex_init(&lock, NULL);
   pthread_cond_init(&cond, NULL);
   pthread_create(&tid1, NULL, (void *)consumer, NULL);
